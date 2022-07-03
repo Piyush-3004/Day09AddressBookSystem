564a40411fddb04c90d410abc454730342942db9
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 23:59:58 +0530

    add new contact

--- src/com/blzAsg/AddressBook.java
+++ src/com/blzAsg/AddressBook.java
@@ -1,15 +1,40 @@
 package com.blzAsg;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		ArrayList<Contact> contactDetails = new ArrayList<Contact>();
 
 		AddressBook.addContact(contactDetails);
+		AddressBook.addNewContact(contactDetails);
+		AddressBook.viewEmp(contactDetails);
+	}
+
+	static void addNewContact(ArrayList<Contact> contactDetails) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter First Name : ");
+		String firstName = scanner.next();
+		System.out.println("Enter Last Name : ");
+		String lastName = scanner.next();
+		System.out.println("Enter Address : ");
+		String address = scanner.next();
+		System.out.println("Enter City : ");
+		String city = scanner.next();
+		System.out.println("Enter State : ");
+		String state = scanner.next();
+		System.out.println("Enter ZipCode : ");
+		int zipCode = scanner.nextInt();
+		System.out.println("Enter Mobile Number : ");
+		long phonenumber = scanner.nextLong();
+		System.out.println("Enter EmailId : ");
+		String emailId = scanner.next();
+		Contact person = new Contact(firstName, lastName, address, city, state, zipCode, phonenumber, emailId);
+		contactDetails.add(person);
+
 	}
 
 	static void addContact(ArrayList<Contact> contactDetails) {
